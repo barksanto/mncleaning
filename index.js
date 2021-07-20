01a9856bbbf8be3715f01684f3dfa12386de2a68
Barkley Santo <barkley.santo@gmail.com>
2021-07-20 14:39:45 -0500

    changes background color to nav menu items

---
+++ index.js
@@ -0,0 +1,15 @@
+// let bigNav = document.querySelector('.big-nav');
+
+
+// function showNav(element){
+//   let window = window.innerWidth;
+
+//   if( window < 844){
+//     element.classList.remove('d-none');
+//   }
+
+//   element.classList.add('d-none');
+// }
+
+// showNav(bigNav)
+
