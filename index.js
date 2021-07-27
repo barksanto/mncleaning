7b03c948b9344ff7fafdb203369bcb8852c26aee
Barkley Santo <barkley.santo@gmail.com>
2021-07-27 16:37:02 -0500

    added formspree page 1 & 2 of form

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// let bigNav = document.querySelector('.big-nav');
-
-
-// function showNav(element){
-//   let window = window.innerWidth;
-
-//   if( window < 844){
-//     element.classList.remove('d-none');
-//   }
-
-//   element.classList.add('d-none');
-// }
-
-// showNav(bigNav)
-
+  var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
