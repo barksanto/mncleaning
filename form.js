a2b994e678b8034347dfa48e37d79630700b58f4
Barkley Santo <barkley.santo@gmail.com>
2021-07-29 17:27:54 -0500

    form works - and modal shows as should

--- form.js
+++ form.js
@@ -100,7 +100,7 @@ let testSubmit = document.querySelector('#test-submit');
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.querySelector(".myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -113,6 +113,7 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  form.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -120,6 +121,6 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
 // hides the form when clicked outside of modal
-    form .style.display = "none"
+    form.style.display = "none";
   }
 }
